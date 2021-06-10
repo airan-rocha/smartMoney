c1a32bbd3eecbfadbe3a0f21b45f35905f64e3c0
airan-rocha <rocha.airan@gmail.com>
2021-06-10 14:59:28 -0300

    Criação do defaultCategories

--- src/schemas/CategorySchema.js
+++ src/schemas/CategorySchema.js
@@ -5,6 +5,7 @@ const CategorySchema = {
         id: 'string',
         name: 'string',
         color: {type: 'string', default: '#fff'},
+        isInit: {type: 'string', default: false},
         isDefault: {type: 'bool', default: false},
         isCredit: {type: 'bool', default: false},
         isDebit: {type: 'bool', default: false},
