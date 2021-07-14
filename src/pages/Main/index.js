8055b96ae2eba0e7c004cf42e81e9a746ba4501f
airan-rocha <rocha.airan@gmail.com>
2021-07-14 10:43:33 -0300

    Ajustando a tela de relatório (Report)

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, ScrollView, LogBox} from 'react-native';
 
 import BalancePanel from '../../components/BalancePanel';
 import EntrySummary from '../../components/EntrySummary';
@@ -8,18 +8,21 @@ import EntryList from '../../components/EntryList';
 import Colors from '../../styles/Colors';
 
 const Main = ({navigation}) => {
+  LogBox.ignoreLogs(['VirtualizedLists should never be']);
   return (
     <View style={styles.container}>
       <BalancePanel onNewEntryPress={() => navigation.navigate('NewEntry')} />
-      <EntrySummary onPressActionButton={() => navigation.navigate('Report')} />
-      <EntryList
-        onEntryPress={entry =>
-          navigation.navigate('NewEntry', {
-            entry: entry,
-          })
-        }
-        onPressActionButton={() => navigation.navigate('Report')}
-      />
+      <ScrollView>
+        <EntrySummary onPressActionButton={() => navigation.navigate('Report')} />
+        <EntryList
+          onEntryPress={entry =>
+            navigation.navigate('NewEntry', {
+              entry: entry,
+            })
+          }
+          onPressActionButton={() => navigation.navigate('Report')}
+        />
+      </ScrollView>
     </View>
   );
 };
